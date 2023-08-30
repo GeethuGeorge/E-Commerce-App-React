e56f8989975418c487839b3ec33b60c2beaae23a
Geethu George <109959204+GeethuGeorge@users.noreply.github.com>
2023-08-30 13:30:02 +0200

    Added Slider and announcment components

--- ecomapp/src/components/Navbar.js
+++ ecomapp/src/components/Navbar.js
@@ -1,6 +1,8 @@
 import { Search } from "@mui/icons-material";
 import React from "react";
-import styled from "styled-components"; // Use `styled` as the default import
+import styled from "styled-components";
+import Badge from '@mui/material/Badge'; // Use `styled` as the default import
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 
 // Styled component-1
 const Container = styled.div`
@@ -18,7 +20,7 @@ const Wrapper = styled.div`
 // Styled component-3
 const Left = styled.div`
     flex: 1;
-    display:flex;
+    display: flex;
     align-items: center;
 `;
 // Styled component-6
@@ -27,23 +29,43 @@ const Language = styled.span`
     cursor: pointer;
 `;
 
-
 // Styled component-7
-const SearchContainer=styled.div`
-border: 0.5px solid lightgray;
-display:flex;
-align-items: center;
-margin-left:25px;
-padding:5px;
-`
+const SearchContainer = styled.div`
+    border: 0.5px solid lightgray;
+    display: flex;
+    align-items: center;
+    margin-left: 25px;
+    padding: 5px;
+`;
+
+//styled component-8
+const Input = styled.input`
+    border: none;
+`;
+//styled component-9
+const Logo = styled.h1`
+    font-weight: bold;
+`;
+
 // Styled component-4
 const Center = styled.div`
     flex: 1;
+
+    text-align: center;
 `;
 
 // Styled component-5
 const Right = styled.div`
     flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+`;
+// Styled component-10
+const MenuItem = styled.div`
+    font-size: 14px;
+    cursor: pointer;
+    margin-left:25px;
 `;
 
 const Navbar = () => {
@@ -53,12 +75,22 @@ const Navbar = () => {
                 <Left>
                     <Language>EN</Language>
                     <SearchContainer>
-                        Input
-                        <Search/>
+                        <Input />
+                        <Search style={{color: "gray", fontSize:16}}/>
                     </SearchContainer>
                 </Left>
-                <Center>Center</Center>
-                <Right>Right</Right>
+                <Center>
+                    <Logo>LUXE</Logo>
+                </Center>
+                <Right>
+                    <MenuItem>REGISTER</MenuItem>
+                    <MenuItem>LOGIN</MenuItem>
+                    <MenuItem>
+                        <Badge badgeContent={4} color="secondary">
+                            <ShoppingCartOutlinedIcon color="action" />
+                        </Badge>
+                    </MenuItem>
+                </Right>
             </Wrapper>
         </Container>
     );
